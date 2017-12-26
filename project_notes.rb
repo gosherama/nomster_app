ff935492aab14aa1b6d850ca2bfeb68f0d45d059
Gosherama <macciak@gmail.com>
2017-12-26 13:24:57 +0000

    initial commit

---
+++ project_notes.rb
@@ -0,0 +1,18 @@
+##NOMSTER##
+
+#! interesting stuff
+#? Questions/check
+#* structure
+#TODO
+
+#* Create rails application
+  # rails new nomster --database=postgresql
+  # Adjust config/database.yml
+  # create initial database
+      rake db:create
+  # test server
+      rails server -b 0.0.0.0 -p 3000
+#* Setting up webdev pipeline
+  # Set up Git
+    #! create a new SSH key
+  # Set up Github
