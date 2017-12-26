166c906bf3314aa50664234af13e8fd81ef92025
Gosherama <macciak@gmail.com>
2017-12-26 14:17:49 +0000

    Added model place & migration

--- project_notes.rb
+++ project_notes.rb
@@ -7,27 +7,27 @@ ruby v2.3.1
 #* structure
 #TODO
 
-#* Create rails application
+#* Create rails application-----------------
   # rails new nomster --database=postgresql
   # Adjust config/database.yml
   # create initial database
       rake db:create
   # test server
       rails server -b 0.0.0.0 -p 3000
-#* Setting up webdev pipeline
+#* Setting up webdev pipeline----------------
   # Set up Git
     #? SSH key management
   # Set up Github
 
-#* Deploying to heroku
+#* Deploying to heroku----------------------
   # heroku create nomster-alex-app
   # git push heroku master
   # heroku apps:info
   #TODO: check basic Heroku ninja course
 
-#* Wireframing
+#* Wireframing------------------------------
 
-#* First page
+#* First page-------------------------------
   # create controller
     rails generate controller places
     #! Rails assumes the controller names are the plural form of the words.
@@ -35,3 +35,10 @@ ruby v2.3.1
   # update route
     root 'places#index'
   # create index.html.erb file and add h1
+
+#* Setting model and DB for places-----------
+  # generate model place & migration file
+    rails generate model place
+    update migration file & run migration
+
+
